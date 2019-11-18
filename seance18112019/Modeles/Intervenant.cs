221e4437ba5fad9326366258dd62a43293720106
legall22610 <thierry28220@icloud.com>
2019-11-18 11:28:33 +0100

    phase2

---
+++ seance18112019/Modeles/Intervenant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seance18112019.Modeles
+{
+    class Intervenant
+    {
+        private int _id;
+    }
+}
